296bd0f21f514eff4c49cc2da03fed56f2b1470c
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-13 08:21:23 -0700

    notes included for reviewers

--- lib/scene_4_module.rb
+++ lib/scene_4_module.rb
@@ -9,18 +9,16 @@ module Scene4
 
   def becky_with_the_good_hair_lines
     text_reader('./data/scene_4.txt').find_all do |line|
-      BECKY_LINES << line if line.include?("BECKY")
-      require "pry"; binding.pry
+      BECKY_LINES << line if line.include?("BECKY") # when it hits that data,
+      #the first line only comes out to [BECKY I under] not sure why
+      # its not gathering the whole line...any thoughts??
     end
   end
 
   def max_lines
     text_reader('./data/scene4.txt').find_all do |line|
       MAX_LINES << line if line.include?("MAX")
-    end.chomp
+    end
   end
 
-  def another_2
-    `say scene 4`
-  end
 end
